8b334b4d552ce76846e36e8139a1d01f8154516a
PauBadiaM <pau.badia@uni-heidelberg.de>
2024-03-22 14:24:43 +0100

    Remove empty cells for pando pre

--- workflow/scripts/methods/pando/pre.py
+++ workflow/scripts/methods/pando/pre.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import mudata as mu
 import argparse
 
@@ -14,7 +15,7 @@ path_input = args['path_input']
 path_peaks = args['path_peaks']
 path_out = args['path_out']
 
-# Read 
+# Read
 mdata = mu.read(path_input)
 df = pd.read_csv(path_peaks)
 
@@ -22,7 +23,17 @@ df = pd.read_csv(path_peaks)
 peaks = df['seqnames'].astype(str) + '-' + df['start'].astype(str) + '-' + df['end'].astype(str)
 
 # Filter
-mdata.mod['atac'] = mdata.mod['atac'][:, peaks].copy()
+atac = mdata.mod['atac'][:, peaks].copy()
+msk = np.sum(atac.X, axis=1) != 0
+atac = atac[msk, :].copy()
+
+# Remove missmatched obs
+rna = mdata.mod['rna'].copy()
+inter = np.intersect1d(rna.obs_names, atac.obs_names)
+mdata = mu.MuData({
+    'rna': rna[inter, :].copy(),
+    'atac': atac[inter, :].copy(),
+})
 
 # Write
 mdata.write(path_out)
