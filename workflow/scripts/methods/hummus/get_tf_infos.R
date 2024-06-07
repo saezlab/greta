67c799c0e23b4a2e02aa55bc6deb652149dc915f
r-trimbour <trimbour@edu.bio.ens.psl.eu>
2024-06-07 14:53:07 +0200

    arboreto from git to match dask recent distribution, grnboost2 and atacnet in the same env than hummus, seurat_object pb fixed by resinstalling tfbstools

--- workflow/scripts/methods/hummus/get_tf_infos.R
+++ workflow/scripts/methods/hummus/get_tf_infos.R
@@ -2,44 +2,29 @@ library(tidyverse)
 library(rhdf5)
 library(HuMMuS)
 
-
 # Parse args
 args <- commandArgs(trailingOnly = F)
 path_data <- args[6]
 organism <- args[7]
 tf_list_file <- args[8]
-tf_motif_file <- args[9]
 
 # Read MuData
 indata <- H5Fopen(path_data, flags='H5F_ACC_RDONLY')
-# RNA
-#rna_X <- Matrix::sparseMatrix(
-#  i = as.vector(indata$mod$rna$X[['indices']][] + 1),
-#  p = as.vector(indata$mod$rna$X[['indptr']][]),
-#  x = as.vector(indata$mod$rna$X[['data']][])
-#)
 
 # RNA
 rna_X <- as.matrix(indata$mod$rna$X)
-
-print(rna_X[1:5, 1:5])
 colnames(rna_X) <- indata$obs$`_index`
 rownames(rna_X) <- stringr::str_replace_all(indata$mod$rna$var$`_index`, '-', '_')
 h5closeAll()
 
-print(Matrix::t(rna_X[1:5,1:5]))
+
 # Create HuMMuS object
 seurat <- SeuratObject::CreateSeuratObject(
-    assay = 'RNA',
-    counts = rna_X[,1:2])
-
-seurat[['RNA']]
-
-print(seurat)
+    assay = "RNA",
+    counts = rna_X[, 1:2])
 hummus <- HuMMuS::Initiate_Hummus_Object(seurat)
 rm(seurat)
 
-hummus[['RNA']]
 
 # Set genome
 if (organism == 'hg38'){
@@ -54,8 +39,3 @@ get_tfs(hummus = hummus,
         store_tfs = TRUE,
         output_file = tf_list_file,
         verbose = 1)
-
-hummus@motifs_db
-
-# store TF motifs
-saveRDS(hummus, tf_motif_file)
