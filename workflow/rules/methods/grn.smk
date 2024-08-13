eb2bd496616c4a96b1e8d19604e9221aaf4f00c8
PauBadiaM <pau.badia@uni-heidelberg.de>
2024-08-13 16:09:52 +0200

    Added prior grns

--- workflow/rules/methods/grn.smk
+++ workflow/rules/methods/grn.smk
@@ -13,3 +13,23 @@ rule get_grn:
         -i {input} \
         -o {output}
         """
+
+rule prc_prior_grn:
+    input:
+        data='datasets/{dataset}/cases/{case}/mdata.h5mu',
+        grn='/mnt/sds-hd/sd22b002/projects/GRETA/greta_resources/database/hg38/grn/{grn_name}.csv',
+        proms='/mnt/sds-hd/sd22b002/projects/GRETA/greta_resources/database/hg38/cre/promoters/promoters.bed',
+    singularity:
+        'workflow/envs/gretabench.sif'
+    output:
+        'datasets/{dataset}/cases/{case}/runs/{grn_name}.grn.csv'
+    resources:
+        mem_mb=16000
+    shell:
+        """
+        python workflow/scripts/methods/prc_prior_grn.py \
+        -g {input.grn} \
+        -d {input.data} \
+        -p {input.proms} \
+        -o {output}
+        """
---
+++ workflow/scripts/methods/prc_prior_grn.py
@@ -0,0 +1,47 @@
+import pyranges as pr
+import pandas as pd
+import numpy as np
+import os
+import mudata as mu
+import argparse
+
+
+# Init args
+parser = argparse.ArgumentParser()
+parser.add_argument('-g','--grn_path', required=True)
+parser.add_argument('-d','--data_path', required=True)
+parser.add_argument('-p','--proms_path', required=True)
+parser.add_argument('-o','--out_path', required=True)
+args = vars(parser.parse_args())
+
+grn_path = args['grn_path']
+data_path = args['data_path']
+proms_path = args['proms_path']
+out_path = args['out_path']
+
+# Read
+grn = pd.read_csv(grn_path)
+genes = mu.read(os.path.join(data_path, 'mod', 'rna')).var_names.astype('U')
+peaks = mu.read(os.path.join(data_path, 'mod', 'atac')).var_names.astype('U')
+proms = pr.read_bed(proms_path)
+
+# Transform peaks
+peaks = pd.DataFrame(peaks, columns=['cre'])
+peaks[['Chromosome', 'Start', 'End']] = peaks['cre'].str.split('-', n=2, expand=True)
+peaks = pr.PyRanges(peaks[['Chromosome', 'Start', 'End']])
+
+# Filter by genes
+grn = grn[grn['source'].astype('U').isin(genes) & grn['target'].astype('U').isin(genes)]
+proms = proms[proms.Name.astype('U').isin(genes)]
+
+# Filter by peaks
+proms = proms.overlap(peaks)
+proms.cre = proms.df['Chromosome'].astype(str) + '-' + proms.df['Start'].astype(str) + '-' + proms.df['End'].astype(str)
+proms = proms.df[['cre', 'Name']].rename(columns={'Name': 'target'})
+
+# Merge
+grn = pd.merge(grn, proms, how='inner')[['source', 'cre', 'target', 'weight']]
+grn = grn.sort_values(['source', 'target', 'cre'])
+
+# Write
+grn.to_csv(out_path, index=False)
