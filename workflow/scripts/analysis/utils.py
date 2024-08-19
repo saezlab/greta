2dbe05955e7756a8f87729857254c8077c71a304
PauBadiaM <pau.badia@uni-heidelberg.de>
2024-08-19 13:12:31 +0200

    ocoeff now makes sure to drop duplicates

--- workflow/scripts/analysis/utils.py
+++ workflow/scripts/analysis/utils.py
@@ -4,9 +4,10 @@ import numpy as np
 
 def ocoeff(df_a, df_b, on=['source', 'target']):
     """Compute overlap coefficient between two dfs"""
-    a_size, b_size = df_a.shape[0], df_b.shape[0]
+    tmp_a, tmp_b = df_a.drop_duplicates(on), df_b.drop_duplicates(on)
+    a_size, b_size = tmp_a.shape[0], tmp_b.shape[0]
     if (a_size > 0) and (b_size > 0):
-        inter = pd.merge(df_a, df_b, on=on, how='inner')
+        inter = pd.merge(tmp_a, tmp_b, on=on, how='inner')
         i_size = inter.shape[0]
         coeff = i_size / np.min([a_size, b_size])
     else:
@@ -14,7 +15,7 @@ def ocoeff(df_a, df_b, on=['source', 'target']):
     return coeff
 
 
-def parallel_ocoeff(index_pair):
+def parallel_ocoeff(index_pair, dfs):
     i, j = index_pair
     tf_oc = ocoeff(dfs[i], dfs[j], on=['source'])
     edge_oc = ocoeff(dfs[i], dfs[j], on=['source', 'target'])
