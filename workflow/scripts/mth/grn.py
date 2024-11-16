a909351011c15f89a22b01000ea68800f0cbe3a4
PauBadiaM <pau.badia@uni-heidelberg.de>
2024-11-16 13:13:19 +0100

    Made grn_run more mem efficient

--- workflow/scripts/mth/grn.py
+++ workflow/scripts/mth/grn.py
@@ -15,7 +15,14 @@ path_out = args['path_out']
 path = os.path.dirname(mdl_path)
 names = os.path.basename(mdl_path)
 lst = names.replace('.mdl.csv', '').split('.')
-mdl = pd.read_csv(mdl_path)
+mdl = pd.read_csv(mdl_path, dtype={'source': 'str', 'target': 'str', 'score': 'float32', 'pval': 'float32'})
+# Skip if empty
+if len(mdl) == 0:
+    grn = pd.DataFrame(columns=['source', 'cre', 'target', 'score', 'pval'])
+    grn.to_csv(path_out, index=False)
+    os._exit(0)
+tfs = set(mdl['source'])
+gns = set(mdl['target'])
 
 # Skip baselines
 baselines = ['collectri', 'dorothea', 'random', 'scenic']
@@ -30,12 +37,15 @@ p2g_path = os.path.join(path, '{pre}.{p2g}.p2g.csv'.format(pre=pre_name, p2g=p2g
 tfb_path = os.path.join(path, '{pre}.{p2g}.{tfb}.tfb.csv'.format(pre=pre_name, p2g=p2g_name, tfb=tfb_name))
 
 # Open files
-tfb = pd.read_csv(tfb_path)
-p2g = pd.read_csv(p2g_path)
+tfb = pd.read_csv(tfb_path, usecols=['tf', 'cre'], dtype={'tf': 'str', 'cre': 'str'})
+tfb = tfb[tfb['tf'].isin(tfs)]
+p2g = pd.read_csv(p2g_path, usecols=['cre', 'gene'], dtype={'cre': 'str', 'gene': 'str'})
+p2g = p2g[p2g['gene'].isin(gns)]
 
 # Merge dfs (can contain duplicates at cre level)
-grn = pd.merge(tfb[['tf', 'cre']], p2g[['cre', 'gene']], on='cre').rename(columns={'tf': 'source', 'gene': 'target'})
+grn = pd.merge(tfb, p2g, on='cre', how='inner').rename(columns={'tf': 'source', 'gene': 'target'})
 grn = pd.merge(grn, mdl, on=['source', 'target'], how='inner').sort_values(['source', 'target', 'cre']).reset_index(drop=True)
+grn = grn[['source', 'cre', 'target', 'score', 'pval']]
 
 # Write
 grn.to_csv(path_out, index=False)
