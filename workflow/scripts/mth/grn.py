6ee19c2d4ed8d5055893b33e664fbfcdbead61c2
PauBadiaM <pau.badia@uni-heidelberg.de>
2025-03-21 11:09:26 +0100

    Optimized grn_run to better handle OOM errors

--- workflow/scripts/mth/grn.py
+++ workflow/scripts/mth/grn.py
@@ -4,8 +4,8 @@ import argparse
 
 # Init args
 parser = argparse.ArgumentParser()
-parser.add_argument('-i','--path_input', required=True)
-parser.add_argument('-o','--path_out', required=True)
+parser.add_argument('-i', '--path_input', required=True)
+parser.add_argument('-o', '--path_out', required=True)
 args = vars(parser.parse_args())
 
 mdl_path = args['path_input']
@@ -15,36 +15,50 @@ path_out = args['path_out']
 path = os.path.dirname(mdl_path)
 names = os.path.basename(mdl_path)
 lst = names.replace('.mdl.csv', '').split('.')
-mdl = pd.read_csv(mdl_path, dtype={'source': 'str', 'target': 'str', 'score': 'float32', 'pval': 'float32'})
+
+# Read in chunks to reduce memory usage
+chunksize = 100_000  # Adjust based on available memory
+dtype_dict = {'source': 'category', 'target': 'category', 'score': 'float32', 'pval': 'float32'}
+mdl_chunks = pd.read_csv(mdl_path, dtype=dtype_dict, chunksize=chunksize)
+mdl = pd.concat(mdl_chunks, ignore_index=True)
+
 # Skip if empty
-if len(mdl) == 0:
+if mdl.empty:
     grn = pd.DataFrame(columns=['source', 'cre', 'target', 'score', 'pval'])
     grn.to_csv(path_out, index=False)
     os._exit(0)
-tfs = set(mdl['source'])
-gns = set(mdl['target'])
+
+# Limit to 100k largest absolute scores
+mdl = mdl.nlargest(100_000, 'score', keep='all').reset_index(drop=True)
+tfs = set(mdl['source'].unique())
+gns = set(mdl['target'].unique())
 
 # Skip baselines
-baselines = ['collectri', 'dorothea', 'random', 'scenic']
-if all(x == lst[0] for x in lst):
-    if (lst[0] in baselines) or lst[0].startswith('o_'):
-        mdl.to_csv(path_out, index=False)
-        os._exit(0)
+baselines = {'collectri', 'dorothea', 'random', 'scenic'}
+if lst[0] in baselines or lst[0].startswith('o_'):
+    mdl.to_csv(path_out, index=False)
+    os._exit(0)
 
-# Read
+# Read paths
 pre_name, p2g_name, tfb_name, mdl_name = lst
-p2g_path = os.path.join(path, '{pre}.{p2g}.p2g.csv'.format(pre=pre_name, p2g=p2g_name))
-tfb_path = os.path.join(path, '{pre}.{p2g}.{tfb}.tfb.csv'.format(pre=pre_name, p2g=p2g_name, tfb=tfb_name))
+p2g_path = os.path.join(path, f'{pre_name}.{p2g_name}.p2g.csv')
+tfb_path = os.path.join(path, f'{pre_name}.{p2g_name}.{tfb_name}.tfb.csv')
 
-# Open files
-tfb = pd.read_csv(tfb_path, usecols=['tf', 'cre'], dtype={'tf': 'str', 'cre': 'str'})
-tfb = tfb[tfb['tf'].isin(tfs)]
-p2g = pd.read_csv(p2g_path, usecols=['cre', 'gene'], dtype={'cre': 'str', 'gene': 'str'})
-p2g = p2g[p2g['gene'].isin(gns)]
+# Read relevant columns with filtering
+usecols_tfb = ['tf', 'cre']
+usecols_p2g = ['cre', 'gene']
 
-# Merge dfs (can contain duplicates at cre level)
-grn = pd.merge(tfb, p2g, on='cre', how='inner').rename(columns={'tf': 'source', 'gene': 'target'})
-grn = pd.merge(grn, mdl, on=['source', 'target'], how='inner').sort_values(['source', 'target', 'cre']).reset_index(drop=True)
+tfb_chunks = pd.read_csv(tfb_path, usecols=usecols_tfb, dtype={'tf': 'category', 'cre': 'category'}, chunksize=chunksize)
+tfb = pd.concat((chunk[chunk['tf'].isin(tfs)] for chunk in tfb_chunks), ignore_index=True)
+
+p2g_chunks = pd.read_csv(p2g_path, usecols=usecols_p2g, dtype={'cre': 'category', 'gene': 'category'}, chunksize=chunksize)
+p2g = pd.concat((chunk[chunk['gene'].isin(gns)] for chunk in p2g_chunks), ignore_index=True)
+
+# Merge in an optimized manner
+grn = tfb.merge(p2g, on='cre', how='inner')
+grn = grn.rename(columns={'tf': 'source', 'gene': 'target'})
+grn = grn.merge(mdl, on=['source', 'target'], how='inner')
+grn = grn.sort_values(['source', 'target', 'cre']).reset_index(drop=True)
 grn = grn[['source', 'cre', 'target', 'score', 'pval']]
 
 grn.to_csv(path_out, index=False)
