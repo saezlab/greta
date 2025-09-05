538480e9e58840cad222d84053c427b1079b041e
Thorben Söhngen <60098461+t-soehngen@users.noreply.github.com>
2025-09-05 16:17:07 +0200

    Added scripts necessary for running scDoRI

---
+++ workflow/scripts/mth/scdori/train_scDORI.py
@@ -0,0 +1,131 @@
+## ToDo: Add a wrapping function that is callable from the wrapper
+
+import logging
+import torch # type: ignore
+import numpy as np
+from torch.utils.data import DataLoader, TensorDataset # type: ignore
+from sklearn.model_selection import train_test_split
+from pathlib import Path
+from sklearn.preprocessing import OneHotEncoder
+
+
+from scdori import ( # type: ignore
+    #trainConfig,
+    load_scdori_inputs,
+    save_model_weights,
+    set_seed,
+    scDoRI,
+    train_scdori_phases,
+    train_model_grn,
+    initialize_scdori_parameters,
+    load_best_model,
+)
+def wrapper_scdori_training(trainConfig):
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=trainConfig.logging_level)
+
+    logger.info("Starting scDoRI pipeline with integrated GRN.")
+    set_seed(trainConfig.random_seed)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    logger.info(f"Using device: {device}")
+
+    rna_metacell, atac_metacell, gene_peak_dist, insilico_act, insilico_rep = (
+        load_scdori_inputs(trainConfig)
+    )
+    gene_peak_fixed = gene_peak_dist.clone()
+    gene_peak_fixed[gene_peak_fixed > 0] = 1  # mask for peak-gene links based on distance
+
+    # computing indices of genes which are TFs and setting number of cells per metacell ( set to 1 for single cell data)
+    rna_metacell.obs["num_cells"] = 1
+    rna_metacell.var["index_int"] = range(rna_metacell.shape[1])
+    tf_indices = rna_metacell.var[rna_metacell.var.gene_type == "TF"].index_int.values
+    num_cells = rna_metacell.obs.num_cells.values.reshape((-1, 1))
+
+    batch_col = trainConfig.batch_col
+    rna_metacell.obs["batch"] = rna_metacell.obs[batch_col].values
+    atac_metacell.obs["batch"] = atac_metacell.obs[batch_col].values
+    # obtaining onehot encoding for technical batch,
+
+    enc = OneHotEncoder(handle_unknown="ignore")
+    enc.fit(rna_metacell.obs["batch"].values.reshape(-1, 1))
+
+    onehot_batch = enc.transform(rna_metacell.obs["batch"].values.reshape(-1, 1)).toarray()
+    enc.categories_
+
+    # 2) Make small train/test sets
+    n_cells = rna_metacell.n_obs
+    indices = np.arange(n_cells)
+    train_idx, eval_idx = train_test_split(indices, test_size=0.2, random_state=42)
+    train_dataset = TensorDataset(torch.from_numpy(train_idx))
+    train_loader = DataLoader(
+        train_dataset, batch_size=trainConfig.batch_size_cell, shuffle=True
+    )
+
+    eval_dataset = TensorDataset(torch.from_numpy(eval_idx))
+    eval_loader = DataLoader(
+        eval_dataset, batch_size=trainConfig.batch_size_cell, shuffle=False
+    )
+    num_genes = rna_metacell.n_vars
+    num_peaks = atac_metacell.n_vars
+
+    num_tfs = insilico_act.shape[1]
+
+    num_batches = onehot_batch.shape[1]
+    model = scDoRI(
+        device=device,
+        num_genes=num_genes,
+        num_peaks=num_peaks,
+        num_tfs=num_tfs,
+        num_topics=trainConfig.num_topics,
+        num_batches=num_batches,
+        dim_encoder1=trainConfig.dim_encoder1,
+        dim_encoder2=trainConfig.dim_encoder2,
+    ).to(device)
+
+    model = train_scdori_phases(
+        model,
+        device,
+        train_loader,
+        eval_loader,
+        rna_metacell,
+        atac_metacell,
+        num_cells,
+        tf_indices,
+        onehot_batch, 
+        trainConfig,
+    )
+
+    # saving the model weight correspoinding to final epoch where model stopped training
+    save_model_weights(model, Path(trainConfig.weights_folder_scdori), "scdori_final")
+
+    # loading the best checkpoint from Phase 1
+    model = load_best_model(
+        model, Path(trainConfig.weights_folder_scdori) / "best_scdori_best_eval.pth", device
+    )
+    initialize_scdori_parameters(
+        model,
+        gene_peak_dist,
+        gene_peak_fixed,
+        insilico_act=insilico_act,
+        insilico_rep=insilico_rep,
+        phase="grn",
+    )
+    # train Phase 2 of scDoRI model, TF-gene links are learnt in this phase and used to reconstruct gene-expression profiles
+    model = train_model_grn(
+        model,
+        device,
+        train_loader,
+        eval_loader,
+        rna_metacell,
+        atac_metacell,
+        num_cells,
+        tf_indices,
+        onehot_batch,
+        trainConfig,
+    )
+    # saving the model weight correspoinding to final epoch where model stopped training
+    save_model_weights(model, Path(trainConfig.weights_folder_grn), "scdori_final")
+
+    logger.info("=== Training pipeline completed successfully ===")
+    return
---
+++ workflow/scripts/mth/scdori/utils.py
@@ -0,0 +1,112 @@
+## Utils:
+
+import yaml
+from pathlib import Path
+import ast
+import argparse
+from scdori import ppConfig, trainConfig # type: ignore
+import numpy as np
+import pandas as pd
+import logging
+
+def aggregate_grn_max_val(grn_act, grn_rep, rna_metacell):
+    """
+    Aggregate GRN activator and repressor matrices by taking the maximum absolute value
+    across the two matrices for each TF-gene pair.
+    Keeps the sign of the values.
+    """
+    stacked = np.stack([grn_rep, grn_act], axis=0)  # shape (2, num_topics, num_tfs, num_genes)
+
+    # Collapse along first two dims
+    stacked = stacked.reshape(-1, 300, 4000) 
+
+    # Find indices of max abs value along 0th axis
+    idx = np.abs(stacked).argmax(axis=0)
+
+    # Extract actual values with sign
+    grn = np.take_along_axis(stacked, idx[None, :, :], axis=0).squeeze(0)  # shape (num_tfs, num_genes)]
+    tf_names = rna_metacell.var[rna_metacell.var.gene_type == "TF"].index.values
+    grn_df = pd.DataFrame(
+        grn, index=tf_names, columns=rna_metacell.var_names
+    )
+    grn_long = grn_df.reset_index(
+        ).melt(
+            id_vars="index", var_name="column", value_name="value"
+        ).sort_values(
+            'value', ascending=True
+        ).rename(
+            columns={'index': 'source', 'column': 'target', 'value': 'score'}
+        )
+
+    return grn_long
+
+def load_config_from_yaml_or_cmdline(yaml_path: str, cmdline_args: dict = None):
+    """
+    Load configuration from a YAML file and override with command line arguments if provided.
+    """
+
+    with open(yaml_path, 'r') as f:
+        cfg = yaml.safe_load(f)
+
+    shared_cfg = cfg.get("scDoRIConfig", {})
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=shared_cfg['logging_level'])
+
+    # Override yaml if command line arguments are provided
+    if cmdline_args:
+        for key, val in cmdline_args.items():
+            if hasattr(ppConfig, key) or hasattr(trainConfig, key) or key in shared_cfg:
+                logger.info(f"Overriding parameter {key} from commandline: {shared_cfg[key]} -> {val}")
+                shared_cfg[key] = val
+            else:
+                raise ValueError(f"Key {key} not found in configuration.")
+    
+    data_dir = Path(shared_cfg.get("data_dir", "."))
+
+    # Resolve paths
+    for key in ["genome_dir", "motif_directory", "model_dir", "output_subdir_name"]:
+        if key in shared_cfg:
+            shared_cfg[key] = Path(data_dir, shared_cfg[key])
+
+
+    if "weight_dir" in shared_cfg:
+        for k in ["weights_folder_scdori", "weights_folder_grn"]:
+            shared_cfg[k] = Path(shared_cfg["weight_dir"], shared_cfg[k])
+
+    if "model_dir" in shared_cfg:
+        for k in ["best_scdori_model_path", "best_grn_model_path"]:
+            shared_cfg[k] = Path(shared_cfg["model_dir"], shared_cfg[k])
+    # Take care of naming mismatches -,-
+    shared_cfg['batch_col'] = shared_cfg.get('batch_key') # Different naming in preprocessing and training config
+    shared_cfg['output_subdir'] = shared_cfg.get('output_subdir_name')
+
+    # Override any default values in ppConfig and trainConfig with shared_cfg
+    for key, val in shared_cfg.items():
+        if hasattr(ppConfig, key):
+            setattr(ppConfig, key, val)
+        if hasattr(trainConfig, key):
+            setattr(trainConfig, key, val)
+
+    return ppConfig, trainConfig
+
+def parse_cmdline_args():
+    """Parse command line arguments and return the YAML file path and a dictionary of overrides.
+    Command line arguments should be in the format key=value, separated by a whitespace.
+    """
+    cmdline_args = {}
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, default=None, help="Path to the YAML configuration file.")
+    known_args, overrides = parser.parse_known_args()
+
+    yaml_file = vars(known_args).get("config")
+    for override in overrides:
+        if "=" not in override:
+            raise ValueError(f"Invalid override: {override}. Use key=value format.")
+        key, val = override.split("=", 1)
+        # Parse value to correct type
+        try:
+            val = ast.literal_eval(val)
+        except:
+            pass
+        cmdline_args[key] = val
+    return yaml_file, cmdline_args
