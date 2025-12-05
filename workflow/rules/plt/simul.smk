4473bf513cb5a864acbfe44d86394c20621e84cb
PauBadiaM <pau.badia@uni-heidelberg.de>
2025-12-05 21:07:48 +0100

    Updated color palette and added simul eval plot

--- workflow/rules/plt/simul.smk
+++ workflow/rules/plt/simul.smk
@@ -3,9 +3,11 @@ localrules: fig_simul
 rule fig_simul:
     threads: 1
     singularity: 'workflow/envs/gretabench.sif'
-    input: rules.topo_simul.output
+    input:
+        topo=rules.topo_simul.output,
+        eval=rules.simul_simul.output,
     output: 'plt/simul/fig.pdf'
     shell:
         """
-        python workflow/scripts/plt/simul/simul.py {input} {output}
+        python workflow/scripts/plt/simul/simul.py {input.topo} {input.eval} {output}
         """
--- workflow/scripts/plt/simul/simul.py
+++ workflow/scripts/plt/simul/simul.py
@@ -2,18 +2,23 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from utils import read_config, savefigs
 
 
-path_simul = sys.argv[1]
-path_out = sys.argv[2]
+path_topo = sys.argv[1]
+path_eval = sys.argv[2]
+path_out = sys.argv[3]
 
-df = pd.read_csv(path_simul)
+# Topo
+df = pd.read_csv(path_topo)
 names = df['name_a'].unique()
 s_mat = df[['name_a', 'name_b', 'tf_oc']].pivot(index='name_a', columns='name_b', values='tf_oc').loc[names, names]
 e_mat = df[['name_a', 'name_b', 'edge_oc']].pivot(index='name_a', columns='name_b', values='edge_oc').loc[names, names]
 t_mat = df[['name_a', 'name_b', 'target_oc']].pivot(index='name_a', columns='name_b', values='target_oc').loc[names, names]
 
-fig, axes = plt.subplots(1, 3, figsize=(9, 3), sharey=True)
+fig_topo, axes = plt.subplots(1, 3, figsize=(9, 3), sharey=True)
 axes = axes.ravel()
 
 ax = axes[0]
@@ -34,4 +39,16 @@ ax.set_title('Genes')
 ax.set_xlabel('')
 ax.set_ylabel('')
 
-fig.savefig(path_out, bbox_inches='tight')
+# Eval
+df = pd.read_csv(path_eval)
+config = read_config()
+palette = config['colors']['nets']
+
+fig_eval, ax = plt.subplots(1, 1, figsize=(3, 3))
+sns.barplot(data=df, x='f01', y='name', hue='name', palette=palette, ax=ax)
+ax.set_xlabel(r'$F_{01}$')
+ax.set_ylabel('')
+
+# Write
+figs = [fig_topo, fig_eval]
+savefigs(figs, path_out)
