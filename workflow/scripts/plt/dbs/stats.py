2c6c451f37d4c0cbf011a9008489336f1433be55
PauBadiaM <pau.badia@uni-heidelberg.de>
2026-02-11 19:47:36 +0100

    Updated db stats plt

--- workflow/scripts/plt/dbs/stats.py
+++ workflow/scripts/plt/dbs/stats.py
@@ -9,13 +9,15 @@ from matplotlib import ticker as mticker
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from utils import savefigs
+from utils import savefigs, read_config
 
 
 def heatmap(df, typ, title, order, figs):
     mat = df[df['type'] == typ].pivot(index='db_a', columns='db_b', values='ocoeff')
     mat = mat.combine_first(mat.T).fillna(1)
     mat = mat.loc[order, order]
+    mat.columns = [dbs_dict[c] for c in mat.columns]
+    mat.index = [dbs_dict[c] for c in mat.index]
     h = ma.Heatmap(mat, cmap='Purples', width=1.5, height=1.5, name=title, label="Overlap\nCoefficient", vmin=0, vmax=1)
     h.add_bottom(mp.Labels(mat.columns))
     h.add_left(mp.Labels(mat.index))
@@ -27,10 +29,14 @@ def heatmap(df, typ, title, order, figs):
 
 
 # Read
-df = pd.read_csv(sys.argv[1])
-oc = pd.read_csv(sys.argv[2])
+#df = pd.read_csv(sys.argv[1])
+oc = pd.read_csv(sys.argv[1])
+config = read_config()
+dbs_dict = config['dbs_names']
 
 # Process
+figs = []
+"""
 meta = df.drop_duplicates(['metric', 'name']).groupby('metric', as_index=False).size()
 meta['metric'] = pd.Categorical(meta['metric'], categories=df['metric'].unique(), ordered=True)
 meta = meta.sort_values('metric').reset_index(drop=True)
@@ -39,6 +45,7 @@ heights = (meta['size'] / meta['size'].max()).values
 
 # Plot
 figs = []
+
 fig, axes = plt.subplots(
     n, 1, figsize=(2.5, n * 1), dpi=150,
     gridspec_kw={'height_ratios': heights}, sharex=True
@@ -79,6 +86,7 @@ for i, metric in enumerate(meta['metric']):
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), frameon=False)
 plt.subplots_adjust(wspace=0, hspace=0.5)
 figs.append(fig)
+"""
 
 order = ['knocktf', 'hpa', 'tfmdb', 'europmc', 'intact', 'chipatlas', 'remap2022', 'unibind']
 heatmap(oc, typ='tf', title='TFs', order=order, figs=figs)
@@ -90,4 +98,4 @@ order = ['chipatlas', 'remap2022', 'unibind', 'encode', 'gwascatalogue', 'blackl
 heatmap(oc, typ='bp', title='Base pairs', order=order, figs=figs)
 
 # Write
-savefigs(figs, sys.argv[3])
+savefigs(figs, sys.argv[2])
