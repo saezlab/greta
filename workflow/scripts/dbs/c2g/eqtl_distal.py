e6b3edf56d90759746f1ce1e0fe1802dabcfbde6
PauBadiaM <pau.badia@uni-heidelberg.de>
2026-03-13 00:44:48 +0100

    Add eqtl distal script

---
+++ workflow/scripts/dbs/c2g/eqtl_distal.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import pyranges as pr
+import numpy as np
+import sys
+
+path_db = sys.argv[1]  #'dbs/hg38/c2g/eqtlcatalogue/eqtlcatalogue.bed.gz'
+path_tss = sys.argv[2]  #'dbs/hg38/cre/promoters/promoters.bed.gz'
+path_out = sys.argv[3]
+
+db = pr.read_bed(path_db).df
+tss = pr.read_bed(path_tss).df
+
+# Get TSS midpoint per gene
+tss['TSS_mid'] = (tss['Start'] + tss['End']) // 2
+tss_mid = tss.groupby('Name')['TSS_mid'].first().reset_index()
+
+# Merge and compute distance
+res = db.merge(tss_mid, on='Name', how='left')
+res['Distance'] = ((res['Start'] + res['End']) // 2 - res['TSS_mid']).abs()
+
+# Filter
+res['Strand'] = np.nan
+res = res[['Chromosome', 'Start', 'End', 'Name', 'Score', 'Strand', 'Distance']]
+
+# Write
+res.to_csv(path_out, index=False, sep='\t', header=None, compression="gzip")
